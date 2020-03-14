724e230e82287cf053cc586991559b30fce7af95
daniel <Gregos@10.0.0.7>
2020-03-15 00:48:37 +0200

    new validation class

---
+++ src/main/java/com/greg/coupons/Utils/Validations.java
@@ -0,0 +1,23 @@
+package com.greg.coupons.Utils;
+
+import java.util.regex.Pattern;
+
+import org.springframework.stereotype.Component;
+@Component
+public class Validations {
+	
+	
+	public boolean isValid(String email) 
+    { 
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+ 
+                            "[a-zA-Z0-9_+&*-]+)*@" + 
+                            "(?:[a-zA-Z0-9-]+\\.)+[a-z" + 
+                            "A-Z]{2,7}$"; 
+                              
+        Pattern pat = Pattern.compile(emailRegex); 
+        if (email == null) 
+            return false; 
+        return pat.matcher(email).matches(); 
+    } 
+
+}
